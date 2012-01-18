8e09ab475061f99c80c166f0783674ffc05ca664
Matt Brennan <mattyb1000@gmail.com>
2012-01-18 20:46:34 +0000

    that not this

--- conf/filters.js
+++ conf/filters.js
@@ -1,10 +1,28 @@
-const less = require("less");
+const less = require("less"),
+path = require("path"),
+fs = require("fs");
 
 module.exports = {
 	".less": {
 		type: "text/css",
-		content: function(chunk) {
-			return less.render(chunk);
+		init: function(file) {
+			var that = this;
+			fs.stat(file,function(e,stat) {
+				that.buf = new Buffer(stat.size);
+			});
+			this.off = 0;
+		},
+		chunk: function(chunk,file) {
+			this.off = this.buff.write(chunk,this.off);
+		},
+		output: function(chunk,file) {
+			var that = this;
+			less.render(this.buf.toString("UTF8"),{
+				paths: [".",path.dirname(file)]
+			},function(e,css) {
+				if(e) return that.emit("error",e);
+				that.emit("done",css);
+			});
 		}
 	}
 };
