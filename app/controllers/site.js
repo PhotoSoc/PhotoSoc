fe97aefe6d5e5f1e6406d9f6447ecbdf7951b592
Matt Brennan <mattyb1000@gmail.com>
2012-01-30 10:22:32 +0000

    add site/about back

--- app/controllers/site.js
+++ app/controllers/site.js
@@ -6,6 +6,9 @@ module.exports = {
 	"index": function() {
 		this.render();
 	},
+	"about": function() {
+		this.render();
+	},
 	"contact": function() {
 		var exec = ["Jeff","Matt","Mark"];
 		this.render({exec:exec});
