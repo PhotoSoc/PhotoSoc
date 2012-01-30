d92b7010bd024019c30a07f9740029b9d8480179
Matt Brennan <mattyb1000@gmail.com>
2012-01-30 13:19:01 +0000

    awesome ajax fb api stuff

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -2,8 +2,29 @@ const list = require("mvc/list.js"),
 models = list.models,
 controllers = list.controllers;
 
+const https = require("https");
+
 module.exports = {
 	"index": function() {
 		this.render();
+	},
+	"list": function() {
+		var action = this, start = new Date;
+		https.get({
+			host: "graph.facebook.com",
+			path: "/warwickphotosoc/events\
+?access_token=AAACPGOnKZAyMBAGSZCAYQRXXLe7dhxysHEofmZBNqeuNpXcjg5ZCGIpaleB5xBfKWFca7Djj6ZCcK9s3bvmKhabzZBicvatoUZD\
+&since=0"
+		}, function(res) {
+			var buf = new Buffer(parseInt(res.headers["content-length"])),
+			off=0;
+			res.on("data",function(chunk) {
+				chunk.copy(buf,off);
+				off += chunk.length;
+			}).on("end",function() {
+				var events = JSON.parse(buf.toString("UTF-8"));
+				action.renderJSON(events.data);
+			});
+		});
 	}
 };
