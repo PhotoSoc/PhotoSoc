d6a3a47248972bb27acf8dcc4a2cb19c56d47886
Matt Brennan <mattyb1000@gmail.com>
2012-02-01 15:34:24 +0000

    mostly working ajax

--- js/plugins.js
+++ js/plugins.js
@@ -2,13 +2,13 @@
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  if(this.console) {
-    arguments.callee = arguments.callee.caller;
-    var newarr = [].slice.call(arguments);
-    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
-  }
+	log.history = log.history || [];   // store logs to an array for reference
+	log.history.push(arguments);
+	if(this.console) {
+		arguments.callee = arguments.callee.caller;
+		var newarr = [].slice.call(arguments);
+		(typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+	}
 };
 
 // make it safe to use console.log always
@@ -18,3 +18,95 @@ window.log = function(){
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+function emitter(spec) {
+	var events = {}, queue = {};
+	return Object.create({
+		on: function(ev,cb) {
+			if(ev in events) {
+				events.push(cb);
+			} else {
+				events[ev] = [cb];
+			}
+			if(ev in queue && queue[ev].length)
+				cb.apply(this,queue[ev].shift());
+			return this;
+		},
+		emit: function() {
+			var args = Array.create(arguments),
+			ev = args.shift();
+
+			if(ev in queue) {
+				queue[ev].push(args);
+			} else {
+				queue[ev] = [args];
+			}
+
+			if(ev in events) events[ev].each(function(cb,i) {
+				cb.apply(this,queue[ev].shift());
+			});
+			return this;
+		}
+	},spec);
+}
+function renderer(path,args,layout) {
+	var that = emitter(),
+	opts = path + "?ajax",
+	old = path;
+
+	args = args || {};
+
+	if(path === "base") return that.emit("render",layout,args);
+	if(layout !== false) opts = {url:path,type:"options"};
+	$.ajax(opts).done(function(data,status,xhr) {
+		var comp, output = "";
+		Object.merge(args,JSON.parse(xhr.getResponseHeader(
+			"X-Template-Params"
+		)));
+
+		try {
+			comp = Tmpl.compile(data);
+		} catch(e) {
+			console.log("compilation error", e.stack);
+			that.emit("error",e);
+			return;
+		}
+		try{
+			output = comp.runInNewContext(
+				Object.merge(Object.clone(args,true),{
+					$: {
+						layout: layout,
+						extend: function(daddy) {
+							path = daddy;
+						},
+						set: function(k,v){
+							args[k]=v;
+						},
+						get: function(k,f) {
+							return k in args ? args[k] : f || "";
+						},
+						exists: function(k) {
+							return k in args;
+						}
+					},
+				})
+			);
+		} catch(e) {
+			console.log("runtime error", e.stack);
+			that.emit("error",e);
+			return;
+		}
+		if(old != path) {
+			renderer(path,args,output).on("render",function(output) {
+				that.emit("render",output,args);
+			}).on("error",function(e){
+				that.emit("error",e);
+			});
+		} else {
+			that.emit("render",output);
+		}
+	}).fail(function(xhr,status,err){
+		that.emit("error",e);
+	});
+	
+	return that;
+}
--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-/* Author: 
+/* Author:
 Matt brennan
 */
 
 
+$(document).ready(function(){
+	if(!History.enabled) {
+		return false;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+		var State = History.getState(); // Note: We are using History.getState() instead of event.state
+		var path = State.data.page;
+		
+		renderer(path,{},false).on("render",function(out,args){
+			$('title').html("Warwick PhotoSoc"+("title" in args ? " &bull; "+args.title : ""));
+			$('section[role="main"]').html(out);
+		}).on("error",function(e){
+			console.log(e);
+		});
+	});
+	$('a').click(function(ev){
+		ev.preventDefault();
+		History.pushState({page:this.href},null,this.href);
+	});
+});
