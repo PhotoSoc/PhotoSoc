b88c48088c0053f4410242297d44c0d29e063ac7
Matt Brennan <mattyb1000@gmail.com>
2012-02-06 11:29:30 +0000

    works now. no idea why

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -6,31 +6,33 @@ const http = require("http"),
 qs = require("querystring");
 
 module.exports = {
-	"index": function(params) {
+	"index": function() {
+		this.render();
+	},
+	"list": function(params) {
 
 		var action = this, start = new Date,
-		path = "services/rest/",
+		path = "/services/rest/",
 		params = {
 			method:"flickr.groups.pools.getPhotos",
 			api_key:"2827b151717a238f8d5d097099ba3cc7",
-			group_id:"696232%40N23",
+			group_id:"696232@N23",
 			format:"json",
 			nojsoncallback:1
 		};
-		http.get({
+		var req = http.get({
 			host: "api.flickr.com",
 			path: path+"?"+qs.stringify(params)
 		}, function(res) {
-			var buf = new Buffer(parseInt(res.headers["content-length"])),
-			off=0;
+			var buf = "";
+			res.setEncoding("UTF8")
 			res.on("data",function(chunk) {
-				chunk.copy(buf,off);
-				off += chunk.length;
+				buf += chunk;
 			}).on("end",function() {
-				console.log(buf.toString())
-				var event = JSON.parse(buf.toString("UTF-8"));
-				action.renderJSON(event);
+				var obj = JSON.parse(buf);
+				action.renderJSON(obj.photos.photo);
 			});
-		});
+		})
+		
 	}
 };
