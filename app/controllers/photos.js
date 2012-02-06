a6dfa8607121359bd3876e5f96063274591484ad
Matt Brennan <mattyb1000@gmail.com>
2012-02-06 10:16:06 +0000

    why is flickr giving me a 400?

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -2,18 +2,24 @@ const list = require("gusto").mvc.list,
 models = list.models,
 controllers = list.controllers;
 
-const http = require("http");
+const http = require("http"),
+qs = require("querystring");
 
 module.exports = {
 	"index": function(params) {
 
 		var action = this, start = new Date,
-		path = "?services/rest/?method=flickr.groups.pools.getPhotos&group_id=696232%40N23",
-		token = "&api_key=2827b151717a238f8d5d097099ba3cc7&format=json&nojsoncallback=1";
-		
+		path = "services/rest/",
+		params = {
+			method:"flickr.groups.pools.getPhotos",
+			api_key:"2827b151717a238f8d5d097099ba3cc7",
+			group_id:"696232%40N23",
+			format:"json",
+			nojsoncallback:1
+		};
 		http.get({
 			host: "api.flickr.com",
-			path: path+token
+			path: path+"?"+qs.stringify(params)
 		}, function(res) {
 			var buf = new Buffer(parseInt(res.headers["content-length"])),
 			off=0;
@@ -21,6 +27,7 @@ module.exports = {
 				chunk.copy(buf,off);
 				off += chunk.length;
 			}).on("end",function() {
+				console.log(buf.toString())
 				var event = JSON.parse(buf.toString("UTF-8"));
 				action.renderJSON(event);
 			});
